e63aa7ab69f03912994981eb575f8e789efe1232
kapza <66725494+BitterAlmondJuice@users.noreply.github.com>
2022-02-16 13:15:05 +0100

    Create HempDog.java

---
+++ HempDog.java
@@ -0,0 +1,17 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
+public class HempDog {
+    public static void main(String[] args) {
+        char min = 97;
+        char max = 122;
+
+        char uno = min;
+        char dos = min;
+        char tres = min;
+        char cuatro = min;
+    }
+
+
+}
